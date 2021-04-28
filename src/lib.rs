e3e8fa967bdd90248db18dd9c4222484942793ae
Rhys Holloway <rhyswilliamholloway@gmail.com>
2021-04-28 14:44:04 -0700

    util stuff moved to main crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-pub extern crate serde;
-
 #[cfg(feature = "random")]
 mod random;
 #[cfg(feature = "random")]
@@ -18,68 +16,4 @@ pub mod hash {
         AHashSet as HashSet,
         AHasher as Hasher,
     };
-}
-
-#[cfg(feature = "util")]
-pub mod text;
-
-#[cfg(feature = "util")]
-mod timer;
-
-#[cfg(feature = "util")]
-mod positions;
-
-#[cfg(feature = "util")]
-mod bounding_box;
-
-#[cfg(feature = "util")]
-pub mod battle;
-
-#[cfg(feature = "util")]
-pub use {
-    timer::Timer,
-    positions::{
-        direction::Direction,
-        coordinate::Coordinate,
-        pixel_offset::PixelOffset,
-        position::Position,
-        global_position::GlobalPosition,
-        destination::Destination,
-    },
-    bounding_box::BoundingBox
-};
-
-#[cfg(all(feature = "tinystr", feature = "util"))]
-pub use positions::location::Location;
-
-#[cfg(feature = "util")]
-pub const WIDTH: f32 = 240.0;
-#[cfg(feature = "util")]
-pub const HEIGHT: f32 = 160.0;
-#[cfg(feature = "util")]
-pub const TILE_SIZE: f32 = 16.0;
-
-#[cfg(feature = "util")]
-pub trait Entity {
-	
-	fn spawn(&mut self);
-	
-	fn despawn(&mut self);
-	
-	fn is_alive(&self) -> bool;
-	
-}
-
-#[cfg(feature = "util")]
-pub trait Reset {
-
-	fn reset(&mut self);
-
-}
-
-#[cfg(feature = "util")]
-pub trait Completable: Reset {
-
-    fn is_finished(&self) -> bool;
-
 }
